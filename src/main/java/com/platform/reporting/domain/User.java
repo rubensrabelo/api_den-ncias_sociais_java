3ef4b739422508b1153d6b22c90dc7c7224dbbd8
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-08 16:24:37 -0300

    Changing id from Long to String type

--- src/main/java/com/platform/reporting/domain/User.java
+++ src/main/java/com/platform/reporting/domain/User.java
@@ -14,7 +14,7 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    private Long id;
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
