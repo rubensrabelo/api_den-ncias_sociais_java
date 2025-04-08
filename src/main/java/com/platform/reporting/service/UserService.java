ecc8456f1f7b52d898f782e9bb87e3b45ce7d222
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-08 16:33:03 -0300

    Starting user service

---
+++ src/main/java/com/platform/reporting/service/UserService.java
@@ -0,0 +1,36 @@
+package com.platform.reporting.service;
+
+import com.platform.reporting.domain.User;
+import com.platform.reporting.repository.UserRepository;
+import com.platform.reporting.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(String id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+    }
+
+    public User save(User user) {
+        return repository.insert(user);
+    }
+
+    public User update(String id, User objUpdate) {
+        User object = repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+        // Continuando aqui
+        return object;
+    }
+}
