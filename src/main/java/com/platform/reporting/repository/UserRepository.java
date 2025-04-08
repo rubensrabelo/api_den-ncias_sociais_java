1148ddfdb848b43256e4a88a28b53d6335da59ff
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-08 16:25:53 -0300

    Extends UserRepository to MongoRepository

--- src/main/java/com/platform/reporting/repository/UserRepository.java
+++ src/main/java/com/platform/reporting/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.platform.reporting.repository;
 
+import com.platform.reporting.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends MongoRepository<User, String> {
 }
