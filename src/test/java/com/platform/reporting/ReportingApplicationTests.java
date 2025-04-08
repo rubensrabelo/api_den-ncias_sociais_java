719c1ad736017aeff2ac1ce5c7ff7477e324eb02
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-08 16:09:06 -0300

    Creating the initial project structure

---
+++ src/test/java/com/platform/reporting/ReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
