9a7d426a8ac865770f5bbe69e6267a98c42da18e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-09 09:02:47 -0300

    Creating UserController

--- src/main/java/com/platform/reporting/controller/UserCollaboratory.java
+++ src/main/java/com/platform/reporting/controller/UserCollaboratory.java
@@ -1,4 +1,51 @@
-package com.platform.reporting;
+package com.platform.reporting.controller;
 
+import com.platform.reporting.domain.User;
+import com.platform.reporting.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/users")
 public class UserCollaboratory {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> objects = service.findAll();
+        return ResponseEntity.ok().body(objects);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable String id) {
+        User object = service.findById(id);
+        return ResponseEntity.ok().body(object);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User object) {
+        object = service.save(object);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(object.getId()).toUri();
+        return ResponseEntity.created(uri).body(object);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable String id, @RequestBody User object) {
+        object = service.update(id, object);
+        return ResponseEntity.ok().body(object);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<User> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
