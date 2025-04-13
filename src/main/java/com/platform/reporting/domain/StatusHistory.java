d852eeba227a6e47bce20635f04ef39c82a551cd
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-13 09:21:49 -0300

    feat(address): add create new class StatusHistory

--- src/main/java/com/platform/reporting/domain/StatusHistory.java
+++ src/main/java/com/platform/reporting/domain/StatusHistory.java
@@ -1,4 +1,67 @@
 package com.platform.reporting.domain;
 
+import com.platform.reporting.domain.enums.StatusEnum;
+
+import java.time.Instant;
+import java.util.Objects;
+
 public class StatusHistory {
+
+    private String id;
+    private StatusEnum status;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public StatusHistory() {}
+
+    public StatusHistory(Instant createdAt, Instant updatedAt) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+
+        this.status = StatusEnum.PENDING;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        StatusHistory that = (StatusHistory) o;
+        return Objects.equals(id, that.id) && status == that.status && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, status, createdAt, updatedAt);
+    }
 }
